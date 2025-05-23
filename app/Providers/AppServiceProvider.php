74a6b993f0bda0591d3c6482c9687223fcf580a2
Teguh Praditya <tegupraditya01@gmail.com>
2025-05-23 10:38:30 +0800

    provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+        
         Gate::define('view-student', function (User $user) {
             if ($user->role === "admin" || $user->role === "guest"){
                 return true;
