3eb3477c8f9326700a76266de31f643e2890f93f
Teguh Praditya <tegupraditya01@gmail.com>
2025-05-23 10:28:52 +0800

    student 2 kali

--- database/seeders/StudentsSeeder.php
+++ database/seeders/StudentsSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-class StudentSeeder extends Seeder
+class StudentsSeeder extends Seeder
 {
 
     public function run(): void
