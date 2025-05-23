95591229d65c7cafbd0a284bca19f63ab4e2ba08
Teguh Praditya <tegupraditya01@gmail.com>
2025-05-23 10:16:01 +0800

    change

--- database/seeders/StudentsSeeder.php
+++ database/seeders/StudentsSeeder.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class MajorsSeeder extends Seeder
+class StudentsSeeder extends Seeder
 {
     /**
      * Run the database seeds.
